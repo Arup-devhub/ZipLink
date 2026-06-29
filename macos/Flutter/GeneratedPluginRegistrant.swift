c0b38dae44a8770745234f912e09a44c23383d74
ARUPANANDA LENKA <arupanandalenka16@gmail.com>
2026-06-29 22:59:41 +0530

    Register SharedPreferencesPlugin in GeneratedPluginRegistrant

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -23,6 +23,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
