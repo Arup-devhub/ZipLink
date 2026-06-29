273a8d556237a87565c9ddff6fc79122552cccd9
ARUPANANDA LENKA <arupanandalenka16@gmail.com>
2026-06-29 22:59:26 +0530

    Register PathProviderPlugin in GeneratedPluginRegistrant

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -21,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
